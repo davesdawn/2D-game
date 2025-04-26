ce27bc8065b369735624409428376106f5a85e0c
Dave Asmah <daveasmah@gmail.com>
2025-04-26 11:14:42 -0400

    Set up JS

---
+++ script.js
@@ -0,0 +1,12 @@
+let lives = 3;
+let message = "Game started!";
+let width = 5;
+let height = 5;
+let area = width * height; // operation for finding the area
+
+
+//get the canvas element
+const canvas = document.getElementById("gameCanvas");
+
+//get the 2D drawing context
+const ctx = canvas.getContext("2d");
